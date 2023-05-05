03cd17881dcde095caf6302912f9bc5e3331e232
alvaro-domatech <alvaro.braz@domatech.com.br>
2023-05-05 12:01:18 -0300

    fix: header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -9,12 +9,12 @@ export function Profile({showProviderData= true}: ProviderProps ){
         <Flex align="center">
                { showProviderData && (
                     <Box mr="4" textAlign="right"> 
-                    <Text>Leonardo borges</Text>
-                    <Text color="gray.300" fontSize="small">leoanrdo.borges.neres@gmail.com</Text>
+                    <Text>Álvaro Braz</Text>
+                    <Text color="gray.300" fontSize="small">alvarobraz83@gmail.com</Text>
                 </Box>
                ) }
 
-                <Avatar size="md" name="Leonardo" src="https://github.com/neresleonardo.png"></Avatar>
+                <Avatar size="md" name="Álvaro Braz" src="https://avatars.githubusercontent.com/u/60791524?s=400&u=725be38cea0da2569dc2c253495873cad76206db&v=4"></Avatar>
         </Flex>
     )
 }
